f6b93d84a50bea0abd7e2eeb7c7dfb0b2191049d
r3g492 <kangggunw3492@gmail.com>
2025-03-10 01:15:39 +0900

    add delta time

--- main.go
+++ main.go
@@ -31,9 +31,14 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+		dt := rl.GetFrameTime()
+		gameLogic(dt)
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 		rl.EndDrawing()
 	}
+}
+
+func gameLogic(dt float32) {
 }
