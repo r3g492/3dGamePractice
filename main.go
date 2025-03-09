68a1531b29ded9a26a07cab96ed02ff7a7bae577
r3g492 <kangggunw3492@gmail.com>
2025-03-10 01:08:31 +0900

    add width and height and console inputs

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
+	"os"
+)
+
+var (
+	windowWidth  int
+	windowHeight int
 )
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+
+	fmt.Print("Enter window width: ")
+	if _, err := fmt.Scan(&windowWidth); err != nil {
+		fmt.Println("Invalid input for width:", err)
+		os.Exit(1)
+	}
+
+	fmt.Print("Enter window height: ")
+	if _, err := fmt.Scan(&windowHeight); err != nil {
+		fmt.Println("Invalid input for height:", err)
+		os.Exit(1)
+	}
+
+	rl.InitWindow(int32(windowWidth), int32(windowHeight), "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
 		rl.EndDrawing()
 	}
 }
